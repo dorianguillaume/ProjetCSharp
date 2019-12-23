b39674878bfd53518958f8e9331988f8f3a81e68
Dorian Guillaume <drnguillaume@gmail.com>
2019-12-23 11:00:34 +0100

    Ajout de la classe Question

--- Quiz/Quiz/Question.cs
+++ Quiz/Quiz/Question.cs
@@ -1,6 +1,30 @@
-﻿namespace Quiz
+﻿using System.Collections.Generic;
+
+namespace Quiz
 {
     internal class Question
     {
+        public string Interrogation { get; }
+
+        public List<string> Answers { get; }
+
+        public List<char> GoodAnswers { get; }
+
+        public Question(List<string> question)
+        {
+            Interrogation = question[0];
+
+            for (int i = 1; i < question.Count; i++)
+            {
+                //Selectionne le premier caractère de la ligne
+                if (question[i][0] == '*')
+                {
+                    GoodAnswers.Add(question[i][1]);
+                    Answers.Add(question[i].Substring(1));
+                }
+                else Answers.Add(question[i]);
+            }
+        }
+
     }
 }
