be9b4df2297e4a730186284d908100d373f4a78d
Dorian Guillaume <drnguillaume@gmail.com>
2019-12-24 09:55:06 +0100

    Commentaires détaillés de Player

--- Quiz/Quiz/Player.cs
+++ Quiz/Quiz/Player.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 
 namespace Quiz
 {
+    /// <summary>
+    /// Classe Player permettant la création de joueur dans le Quiz
+    /// </summary>
     public class Player
     {
+        /// <summary>
+        /// Propriété --> nom du joueur
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Propriété --> Prénom du joueur
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Propriété --> Date à laquelle le joueur a effectué son quiz
+        /// </summary>
         public DateTime Date { get; set; }
 
+        /// <summary>
+        /// Propriété --> Résultat du joueur après avoir effectué le quiz
+        /// </summary>
         public int Score { get; set; }
 
+        /// <summary>
+        /// Propriété --> Liste d'entier correspondant aux numéros de question dans lequel le joueur s'est trompé
+        /// </summary>
         public List<int> Errors { get; set; }
 
+        /// <summary>
+        /// Constructeur --> Création d'un joueur avec nom et prénom
+        /// </summary>
+        /// <param name="name">string --> nom du joeur en majuscule</param>
+        /// <param name="firstname">string --> prénom du joueur avec la première lettre en majuscule</param>
         public Player(string name, string firstname)
         {
             Name = name.ToUpper();
@@ -23,8 +46,13 @@ namespace Quiz
             Date = DateTime.Now;
         }
 
+        /// <summary>
+        /// Constructeur --> Utile pour la classe Stats (car les joueurs ne sont pas stockés en BDD)
+        /// </summary>
+        /// <param name="ligne">Création d'un joueur par ligne</param>
         public Player(string ligne)
         {
+            //On divise chaque ligne par tabulation
             string[] tab = ligne.Split('\t');
                        
             Date = DateTime.Parse(tab[0]);
@@ -32,15 +60,22 @@ namespace Quiz
             FirstName = tab[2];
             Score = Int32.Parse(tab[3].Substring(0,1));
             Errors = new List<int>();
+
+            //Pour chaque erreur
             foreach (var item in tab[4].Split(", "))
             {
+                //On s'assure qu'il existe des erreurs
                 if (item != string.Empty)
                 {
                     Errors.Add(Int32.Parse(item));
-                                    }
+                }
             }
         }
 
+        /// <summary>
+        /// Affichage de l'objet Player
+        /// </summary>
+        /// <returns>Une chaine de caractère avec toutes les infos du joueur</returns>
         public override string ToString()
         {
             int nbQuestions = Score + Errors.Count;
